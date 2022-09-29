881b70851813cbde8143ae80966b21ae4a27e505
JxanPadilla <juanlm101120@gmail.com>
2022-09-29 14:22:57 -0500

    Calculadoraexpress

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Main from "./components/Main";
+import Calculadora from "./components/Calculadora";
+import "./assets/css/estilos.css"
+import "./assets/css/estilosmain.css"
+
+function App() {
+  return (
+    <>
+
+      <Main/> 
+      <Calculadora />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const Main = () => {
+  return (
+    <>
+
+      <div id='container'>
+        <div className='titulo'><h1>Calculadora Juniorista☆</h1></div>
+      </div>
+    </> 
+  )
+}
+
+export default Main
